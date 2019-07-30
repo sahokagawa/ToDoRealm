49a05fd585ed15fd1651126ff88e9beecc1591c2
sahokagawa <karaageyuko@gmail.com>
2019-07-30 11:50:47 +0800

    画面遷移

--- ToDoProject/controllers/ViewController.swift
+++ ToDoProject/controllers/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func didClickAddButton(_ sender: UIButton) {
+        performSegue(withIdentifier: "toNext", sender: nil)
+    }
+    
 }
 
--- ToDoProject/objects/Todo.swift
+++ ToDoProject/objects/Todo.swift
@@ -6,4 +6,15 @@
 //  Copyright © 2019 香川紗穂. All rights reserved.
 //
 
-import Foundation
+//import Foundation
+import RealmSwift
+
+//必ず、objectを継承させる
+class Todo: Object {
+//    ID（連番）
+    @objc dynamic var id: Int = 0
+//    タイトル
+    @objc dynamic var title: String = ""
+//    登録日時
+    @objc dynamic var date: Date = Date()
+}
